a3c0c945b5335075dae2788997314e36f47430f2
Stefan Sundin <git@stefansundin.com>
2022-11-19 17:41:16 -0800

    Rename branch.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
-if [[ ! -d master ]]; then
-  echo "Please clone the master branch into a subdirectory:"
+if [[ ! -d main ]]; then
+  echo "Please clone the main branch to a subdirectory:"
   echo
-  echo "  git clone git@github.com:stefansundin/awsebcli.git master"
+  echo "  git clone git@github.com:stefansundin/awsebcli.git main"
   echo
   exit 1
 fi
@@ -15,7 +15,7 @@ author="$GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL>"
 
 cat api.json | jq -Mr '.releases | to_entries | sort_by(.value[0].upload_time+"Z" | fromdateiso8601)[] | [.key, .value[0].upload_time, .value[0].filename, .value[0].url] | join(" ")' | while read -r version upload_time filename url; do
   tag="v$version"
-  if git -C master rev-parse "$tag" >/dev/null 2>&1; then
+  if git -C main rev-parse "$tag" >/dev/null 2>&1; then
     # tag already exists
     continue
   fi
@@ -23,17 +23,17 @@ cat api.json | jq -Mr '.releases | to_entries | sort_by(.value[0].upload_time+"Z
   [[ ! -f "$filename" ]] && wget -O "$filename" "$url"
   echo "$version $filename"
 
-  # delete everything in the master directory except the .git directory
-  find master -path master/.git -prune -o -type f -print | grep -v '^master$' | xargs rm -f
+  # delete everything in the main directory except the .git directory
+  find main -path main/.git -prune -o -type f -print | grep -v '^main$' | xargs rm -f
 
   # extract the new version, commit it, and tag it
-  tar -xzf "$filename" -C master --strip-components=1
-  git -C master add .
-  git -C master commit --no-verify --author="$author" --date="$upload_time" -m "$version"
-  git -C master tag "$tag"
-  git -C master --no-pager log --stat -n 1
+  tar -xzf "$filename" -C main --strip-components=1
+  git -C main add .
+  git -C main commit --no-verify --author="$author" --date="$upload_time" -m "$version"
+  git -C main tag "$tag"
+  git -C main --no-pager log --stat -n 1
   echo
 done
 
 echo
-echo "Manually verify the git log and then run 'git push' and 'git push --tags' in the master directory."
+echo "Manually verify the git log and then run 'git push' and 'git push --tags' in the main directory."
